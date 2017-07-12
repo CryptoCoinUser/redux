2ca2f1e0d5bde2f15640c80c3880853dc526097d
CryptoCoinUser <adkantor@gmail.com>
2017-07-11 18:53:38 -0700

    repo rating example

--- js/index.js
+++ js/index.js
@@ -1,2 +1,32 @@
 require('babel-polyfill');
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
+import store from './store';
+import RepositoryList from './components/repository-list';
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(
+        <Provider store={store}>
+            <RepositoryList />
+        </Provider>,
+        document.getElementById('app')
+    )
+);
+
+
+
+{/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RepositoryList from './components/repository-list';
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(<RepositoryList />, document.getElementById('app'))
+);
+*/}
+
+
